40218ca34c3dac5615c414c5fc874265271bbc7a
wenha2010 <155805773+wenha2010@users.noreply.github.com>
2025-05-22 22:04:53 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -114,40 +114,40 @@ const API_SITES = {
         name: '空内容测试源',
         adult: true
     },
-    ckzy: {
-         api: 'https://www.ckzy1.com',
-         name: 'CK资源',
-     },
-     jkun: {
-         api: 'https://jkunzyapi.com',
-         name: 'jkun资源',
-     },
-     bwzy: {
-         api: 'https://api.bwzym3u8.com',
-         name: '百万资源',
-     },
-     souav: {
-         api: 'https://api.souavzy.vip',
-         adult: true
-     },
-     r155: {
-         api: 'https://155api.com',
-         name: '155资源',
-     },
-     lsb: {
-         api: 'https://apilsbzy1.com',
-         name: 'lsb资源',
-         adult: true
-     },
-     huangcang: {
-         api: 'https://hsckzy.vip',
-         name: '黄色仓库',
-         detail: 'https://hsckzy.vip'
-     },
-     yutu: {
-         api: 'https://yutuzy10.com',
-         name: '玉兔资源',
-     },
+    //ckzy: {
+         //api: 'https://www.ckzy1.com',
+         //name: 'CK资源',
+     //},
+     //jkun: {
+         //api: 'https://jkunzyapi.com',
+         //name: 'jkun资源',
+     //},
+     //bwzy: {
+         //api: 'https://api.bwzym3u8.com',
+         //name: '百万资源',
+     //},
+     //souav: {
+         //api: 'https://api.souavzy.vip',
+         //adult: true
+     //},
+     //r155: {
+         //api: 'https://155api.com',
+         //name: '155资源',
+     //},
+     //lsb: {
+         //api: 'https://apilsbzy1.com',
+         //name: 'lsb资源',
+         //adult: true
+     //},
+     //huangcang: {
+         //api: 'https://hsckzy.vip',
+         //name: '黄色仓库',
+         //detail: 'https://hsckzy.vip'
+     //},
+     //yutu: {
+         //api: 'https://yutuzy10.com',
+         //name: '玉兔资源',
+     //},
 
     // 下面是资源失效率高的API源，不建议使用
     // subo: {
